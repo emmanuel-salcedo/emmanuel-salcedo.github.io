d641256d287452feb7901ee152a658a08e03a7df
emmanuel-salcedo <mail@emmanuelsalcedo.com>
2024-05-23 15:56:37 -0700

    Added Intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <h1>Hello World </h1>
+      <Intro />
     </main>
   )
 }
