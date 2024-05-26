f2e2ba309366b8d01c7b54eea42b7e33680f7890
emmanuel-salcedo <mail@emmanuelsalcedo.com>
2024-05-25 17:00:13 -0700

    test

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,8 +2,8 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { motion } from 'framer-motion';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
