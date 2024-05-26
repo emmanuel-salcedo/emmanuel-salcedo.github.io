0248704d0a1dedd67985643b86645c3c84fb6a52
emmanuel-salcedo <mail@emmanuelsalcedo.com>
2024-05-26 15:18:57 -0700

    add projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,20 @@
+import Project from '@/components/project';
+import Projects from '@/components/projects';
+import SectionHeading from '@/components/section-heading';
+import { projectsData } from '@/lib/data';
 import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Welcome to the About page!</p>
+    <div className="flex flex-col items-center gap-8 p-8">
+      <h1 className="text-4xl font-bold mb-12 text-primary dark:text-primary-dark">My Professional Journey</h1>
+      <div className="relative w-full max-w-3xl">
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
